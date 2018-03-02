fea705140ff7811770743563c039e8154b481a9e
Qichen <yydcqc@163.com>
2018-03-02 22:06:26 +0800

    basic UI design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
+
+export interface Element {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: '9:18', name: '9:20', weight: '9:25', symbol: '9:30'},
+  {position: '9:28', name: '10:20', weight: '10:25', symbol: '10:30'},
+  {position: '9:38', name: '10:30', weight: '10:35', symbol: '10:40'},
+  {position: '9:48', name: '10:40', weight: '10:45', symbol: '10:50'},
+  {position: '9:58', name: '10:50', weight: '11:05', symbol: '11:10'},
+  {position: '9:18', name: '9:20', weight: '9:25', symbol: '9:30'},
+  {position: '9:28', name: '10:20', weight: '10:25', symbol: '10:30'},
+  {position: '9:38', name: '10:30', weight: '10:35', symbol: '10:40'},
+  {position: '9:48', name: '10:40', weight: '10:45', symbol: '10:50'},
+  {position: '9:58', name: '10:50', weight: '11:05', symbol: '11:10'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatListModule,
+  MatCardModule,
+  MatTableModule,
+  MatFormFieldModule
+} from '@angular/material';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    MatIconModule,
+    MatToolbarModule,
+    BrowserModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
